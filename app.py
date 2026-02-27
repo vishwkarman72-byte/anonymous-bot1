ec54ec79ed2d9e2ddb9433c475a02f5e1670ee44
vishwkarman72-byte <vishwkarman72@gmail.com>
2026-02-27 20:39:04 +0530

    Switch from Flask webhook to polling method
    Removed Flask integration and replaced it with polling.

--- app.py
+++ app.py
@@ -1,26 +1,13 @@
 import os
-from flask import Flask, request
 import telebot
 
 TOKEN = os.environ.get("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
-app = Flask(__name__)
-
-@app.route(f"/{TOKEN}", methods=["POST"])
-def webhook():
-    json_str = request.get_data().decode("UTF-8")
-    update = telebot.types.Update.de_json(json_str)
-    bot.process_new_updates([update])
-    return "OK", 200
-
-@app.route("/")
-def home():
-    return "Bot Running 🚀"
-
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, "Bot is working 🚀🔥")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+print("Bot started...")
+
+bot.infinity_polling()
